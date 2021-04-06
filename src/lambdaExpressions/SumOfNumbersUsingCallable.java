076457fe7a8d027bf8b1abd5d9f21838a15850c3
Jaqueline <jaquelinesv90@gmail.com>
2021-04-05 22:01:51 -0300

    updated class

--- src/lambdaExpressions/SumOfNumbersUsingCallable.java
+++ src/lambdaExpressions/SumOfNumbersUsingCallable.java
@@ -1,6 +1,11 @@
 package lambdaExpressions;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 
 public class SumOfNumbersUsingCallable {
@@ -9,7 +14,7 @@ public class SumOfNumbersUsingCallable {
 	public static int total = IntStream.rangeClosed(0, 5000).sum();
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Callable callable1 = () -> {
 			int sum = 0;
 			for(int i =0; i< array.length;i++) {
@@ -25,6 +30,14 @@ public class SumOfNumbersUsingCallable {
 			}
 			return sum;
 		};
+		
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		List<Callable<Integer>> taskList = Arrays.asList(callable,callable1);
+		
+		List<Future<Integer>> results = executorService.invokeAll(taskList);
+		
+		int k =0;
+		int sum =0;
 		
 	}
 	
