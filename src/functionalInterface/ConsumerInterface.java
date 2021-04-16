6106887f2da9e2bbe2823fff1b54d9459ad1f30f
Jaqueline <jaquelinesv90@gmail.com>
2021-04-15 22:37:44 -0300

    updated

--- src/functionalInterface/ConsumerInterface.java
+++ src/functionalInterface/ConsumerInterface.java
@@ -1,6 +1,13 @@
 package functionalInterface;
 
+/* - Consumer interface is a part of the java.util.function package
+ * - It represents a function which takes in one argument and produces a result
+ */
 
 public class ConsumerInterface {
+	
+	public static void main(String[] args) {
+		Consumer<String> c = (x) -> System.out.println()
+	}
 
 }
