feb0e3985ec7d2763a7ce5d76001e1d5af7fe89a
Jaqueline <jaquelinesv90@gmail.com>
2021-04-19 15:54:35 -0300

    updated documentation

--- src/functionalInterface/ConsumerInterface.java
+++ src/functionalInterface/ConsumerInterface.java
@@ -1,13 +1,24 @@
 package functionalInterface;
 
+import java.util.function.Consumer;
+
 /* - Consumer interface is a part of the java.util.function package
- * - It represents a function which takes in one argument and produces a result
+ * - It represents a function which takes in one argument and produces a result.However
+ * these kind of functions don't return any value.
  */
 
 public class ConsumerInterface {
 	
 	public static void main(String[] args) {
-		Consumer<String> c = (x) -> System.out.println()
+		Consumer<String> c = (x) -> System.out.println(x.length() + "the value of x: "+ x);
+		c.accept("up in the air");
+		
+		//Consumer with block statement
+		Consumer<Integer> d = (x) -> {
+			System.out.println("x*x = " + x*x);
+			System.out.println("x/x = " + x/x);
+		};
+		d.accept(10);
 	}
 
 }
